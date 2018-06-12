fd8b9145822cbc3ca2736957daae3963e40e4499
Leo Liu <liuxin19900213@gmail.com>
2018-06-12 11:18:48 +0800

    增加1.23代码

---
+++ books/cpp-primer-5/chapter-1/ex1_23.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "../include/Sales_item.h"
+
+int main()
+{
+    Sales_item currItem, item;
+    
+    if (std::cin >> currItem)
+    {
+        int cnt = 1;
+        while (std::cin >> item)
+        {
+            if (item.isbn() == currItem.isbn())
+                ++cnt;
+            else
+            {
+                std::cout << currItem.isbn() << " occurs "
+                          << cnt << " times" << std::endl;
+                currItem = item;
+                cnt = 1;
+            }
+        }
+
+        std::cout << currItem.isbn() << " occurs "
+                  << cnt << " times" << std::endl;
+        return 0;
+    }
+    else
+    {
+        std::cerr << "No data?!" << std::endl;
+        return -1;
+    }
+}
