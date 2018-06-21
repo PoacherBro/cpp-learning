df34da75b32b61080c6f9a2a5a76600e2ba30eb7
Leo Liu <liuxin19900213@gmail.com>
2018-06-21 10:54:45 +0800

    增加其他指针声明

--- books/cpp-primer-5/chapter-2/pointers.cc
+++ books/cpp-primer-5/chapter-2/pointers.cc
@@ -3,9 +3,13 @@
 int main()
 {
     int errNumb = 0;
-    // 指向常量的指针 low-level const
+    // 指向可变对象的常量指针 top-level const
     int *const curErr = &errNumb;
     const double pi = 3.14159;
     // 指向常量的常量指针 top-level const
     const double *const pip = &pi;
+
+    const int e = 0;
+    // 指向对象是一个常量 low-level const
+    const int *pe = &e;
 }
