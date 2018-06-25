20b6128e4d1be032c5baf7784e0c3eda0310daac
Leo Liu <liuxin19900213@gmail.com>
2018-06-25 15:15:18 +0800

    auto类型推断测试代码

--- books/cpp-primer-5/chapter-2/auto.cc
+++ books/cpp-primer-5/chapter-2/auto.cc
@@ -24,4 +24,23 @@ int main()
     d = 42; // ERROR, d是int指针
     e = 42; // ERROR, e是指向const int的指针
     g = 42; // ERROR, g是整型常量引用
+
+    auto k = ci, &l = i;
+    auto &m = ci, *p = &ci;
+    auto &n = i, *p2 = &ci; // ERROR, i的类型是int，而&ci的类型是const int
+
+    // 推测变量类型
+    const int ti = 42;
+    
+    auto tj = ti; // int类型
+    std::cout << typeid(tj).name() << std::endl;
+
+    const auto &tk = ti; // const int引用类型
+    std::cout << typeid(tk).name() << std::endl;
+
+    auto *tp = &ti; // 指向const int的指针，顶层const
+    std::cout << typeid(tp).name() << std::endl;
+
+    const auto tj2 = ti, &tk2 = ti; // j2是const int类型，k2是绑定const int的常量引用
+    std::cout << typeid(tj2).name() << " " << typeid(tk2).name() << std::endl;
 }
