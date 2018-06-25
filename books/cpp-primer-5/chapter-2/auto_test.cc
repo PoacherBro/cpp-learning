f6355cc074d42839e810cf8962f369f146753113
Leo Liu <liuxin19900213@gmail.com>
2018-06-25 19:46:10 +0800

    格式化auto_test.cc

--- books/cpp-primer-5/chapter-2/auto_test.cc
+++ books/cpp-primer-5/chapter-2/auto_test.cc
@@ -1,20 +1,21 @@
 #include <iostream>
+#include <typeinfo>
 
 int main()
 {
     // 推测变量类型
-     const int ti = 42;
-     
-     auto tj = ti; // int类型
-     std::cout << typeid(tj).name() << std::endl;
+    const int ti = 42;
+    
+    auto tj = ti; // int类型
+    std::cout << typeid(tj).name() << std::endl;
 
     const auto &tk = ti; // const int引用类型
-     std::cout << typeid(tk).name() << std::endl;
+    std::cout << typeid(tk).name() << std::endl;
  
-     auto *tp = &ti; // 指向const int的指针，顶层const
-     std::cout << typeid(tp).name() << std::endl;
+    auto *tp = &ti; // 指向const int的指针，顶层const
+    std::cout << typeid(tp).name() << std::endl;
  
-     const auto tj2 = ti, &tk2 = ti; // j2是const int类型，k2是绑定const int的常量引用
-     std::cout << typeid(tj2).name() << " " << typeid(tk2).name() << std::endl;
+    const auto tj2 = ti, &tk2 = ti; // j2是const int类型，k2是绑定const int的常量引用
+    std::cout << typeid(tj2).name() << " " << typeid(tk2).name() << std::endl;
  
 }
