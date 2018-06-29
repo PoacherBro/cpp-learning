006e55d7bd2f3864e5dbe761e9f9ccc3019d3243
Leo Liu <liuxin19900213@gmail.com>
2018-06-29 21:34:07 +0800

    标准输入输出流操作string

--- books/cpp-primer-5/chapter-3/string.cc
+++ books/cpp-primer-5/chapter-3/string.cc
@@ -2,6 +2,10 @@
 #include <string>
 
 using std::string;
+using std::cout;
+using std::cin;
+using std::endl;  
+// using std::cout, std::cin; 使用逗号分隔来一行声明所有，需要C++17才支持
 
 int main()
 {
@@ -9,6 +13,25 @@ int main()
     string s2 = s1; // s2是s1的副本；拷贝初始化
     string s3 = "hiya";  // s3是该字符串字面值的副本；拷贝初始化
     string s4(10, 'c');  // s4的内容是 cccccccccc；直接初始化
+
+    string s;
+    cin >> s; // 遇到空白停止
+    cout << s << endl;
+
+    cin >> s1 >> s2;
+    cout << s1 << " " << s2 << endl;
+
+   // string word;
+   // while (cin >> word)
+   //     cout << word << endl;
+
+    string line;
+    while (getline(cin, line))
+    {
+        size_t size = line.size();
+        if (!line.empty())
+            cout << line << ", size: " << size << endl;
+    }
 
     return 0;
 }
