88c26f4c8505978033e8581642ac5c6d3f09345e
Leo Liu <liuxin19900213@gmail.com>
2018-06-29 23:54:47 +0800

    增加string相关测试代码

--- books/cpp-primer-5/chapter-3/string.cc
+++ books/cpp-primer-5/chapter-3/string.cc
@@ -7,7 +7,23 @@ using std::cin;
 using std::endl;  
 // using std::cout, std::cin; 使用逗号分隔来一行声明所有，需要C++17才支持
 
-int main()
+void convert_digit_to_hexadecimal()
+{
+    const string hexdigits = "0123456789ABCDEF"; // 可能的十六进制数字
+    cout << "Enter a series of numbers between 0 and 15"
+         << " separated by spaces. Hit ENTER when finished: "
+         << endl;
+
+    string result;
+    string::size_type n;
+    while (cin >> n)
+        if (n < hexdigits.size())
+            result += hexdigits[n];
+
+    cout << "Your hex number is: " << result << endl;
+}
+
+int test()
 {
     string s1; // 默认初始化，s1是一个空字符串；直接初始化
     string s2 = s1; // s2是s1的副本；拷贝初始化
@@ -32,6 +48,17 @@ int main()
         if (!line.empty())
             cout << line << ", size: " << size << endl;
     }
+
+    cout << "垂直制表符\v效果" << endl;
+    cout << "换页符\f效果" << endl;
+
+    return 0;
+}
+
+int main()
+{
+    convert_digit_to_hexadecimal();
+    // test();
 
     return 0;
 }
